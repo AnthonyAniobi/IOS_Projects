dd95131d7de858dfb4aac30b4f55838dc9a6e5ec
Anthony Aniobi <anthonyaniobi198@gmail.com>
2023-03-18 15:09:59 +0100

    initial commit

---
+++ Bullseye/Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Anthony Aniobi on 18/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
