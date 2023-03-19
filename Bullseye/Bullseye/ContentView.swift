2488002a18d74ad6599d1658ba2ee5db261ddc84
Anthony Aniobi <anthonyaniobi198@gmail.com>
2023-03-19 06:21:36 +0100

    add random number feature

--- Bullseye/Bullseye/ContentView.swift
+++ Bullseye/Bullseye/ContentView.swift
@@ -8,28 +8,91 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var alertIsVisible:Bool = false
+    // properties
+    // ..
     
+    // User interface views
+    @State var alertIsVisible:Bool = false
+    @State var sliderValue:Double = 50.0
+    @State var target:Int = Int.random(in: 1...100)
+    var sliderValueRounded : Int{
+        Int(sliderValue.rounded())
+    }
+    
+    // User interface content and layout
     var body: some View {
         VStack {
-            Text("Welcome to my first app!")
-                .fontWeight(.black)
-            .foregroundColor(.green)
+            // target row
+            Spacer()
+            HStack {
+                Text("Put the bullseye close as you can to:")
+                Text("\(self.target)")
+            }
+            // slider row
+            Spacer()
+            HStack{
+                Text("1")
+                Slider(value: self.$sliderValue, in: 1...100)
+                Text("100")
+            }
+            
+            // Button row
+            Spacer()
             Button(action: {
                 print("Button pressed!!")
                 self.alertIsVisible = true
             }){
-                Text("Button").fontWeight(.black).padding()
+                Text("Hit me")
             }.alert(isPresented: self.$alertIsVisible){
-                Alert(title: Text("Hello there!"), message: Text("This is my first pop-up."), dismissButton: .default(Text("Awesome")))
+                Alert(title: Text("Hello there!"), message: Text(self.scoringMessage()), dismissButton: .default(Text("Awesome")))
             }
+            
+            // score row
+            // Todo add views for the score, rounds, start and info buttons here
+            Spacer()
+            HStack {
+                Button(action: {}) {
+                    Text("Start over")
+                }
+                Spacer()
+                Text("Score:")
+                Text("999999")
+                Spacer()
+                Text("Round:")
+                Text("999")
+                Spacer()
+                Button(action: {}) {
+                    Text("Info")
+                }
+            }.padding(.bottom, 20)
         }
             
     }
+    // Methods
+    // ========
+    func pointsForCurrentRound()->Int{
+        var difference:Int
+        if self.target > self.sliderValueRounded{
+            difference = self.target - self.sliderValueRounded
+        }else if self.target < self.sliderValueRounded{
+            difference = self.sliderValueRounded - self.target
+        }else{
+            difference = 0
+        }
+        return 100 - difference
+    }
+    func scoringMessage()->String{
+        return "The slider's value is \(self.sliderValueRounded)\n"+" you scored \(pointsForCurrentRound())"+" points for this round"
+    }
+    
+    
 }
 
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.landscapeRight)
     }
 }
+#endif
