3594b998a66d69560878f9859db0f5d3418da580
Anthony Aniobi <anthonyaniobi198@gmail.com>
2023-07-24 22:43:41 +0100

    feat: Add homepage and transaction model

--- Expence Tracker/Expence Tracker/TransactionRow.swift
+++ Expence Tracker/Expence Tracker/TransactionRow.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct TransactionRow: View {
+    var transaction: Transaction
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 20){
+            VStack(alignment: .leading, spacing: 6) {
+                // MARK: Transaction merchant
+                Text(transaction.merchant)
+                    .font(.subheadline)
+                    .bold()
+                    .lineLimit(1)
+                
+                // MARK: Transaction cateogry
+                Text(transaction.category)
+                    .font(.footnote)
+                    .opacity(0.7)
+                    .lineLimit(1)
+                
+                // MARK: Transaction date
+                Text(transaction.date)
+            }
+        }
+        .padding([.top, .bottom], 8)
+            
     }
 }
 
 struct TransactionRow_Previews: PreviewProvider {
     static var previews: some View {
-        TransactionRow()
+        TransactionRow(transaction: transactionPreviewData)
+//        TransactionRow(transaction: transactionPreviewData)
+//            .preferredColorScheme(.dark)
     }
 }
