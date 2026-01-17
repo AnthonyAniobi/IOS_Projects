55fc5659e1204d1e6df8d01744f9da2641b5c346
Anthony Aniobi <anthonyaniobi198@gmail.com>
2026-01-17 04:14:40 +0100

    setup new map project

---
+++ MapBookmarkApp/MapBookmarkApp/MapBookmarkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapBookmarkAppApp.swift
+//  MapBookmarkApp
+//
+//  Created by Anthony Aniobi on 17/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MapBookmarkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
