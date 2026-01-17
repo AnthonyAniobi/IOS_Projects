2a2d327913a062f6ff08fa7f2323c9f7a0c4eb25
Anthony Aniobi <anthonyaniobi198@gmail.com>
2026-01-17 04:21:05 +0100

    add location dataservice and location model

---
+++ MapBookmarkApp/MapBookmarkApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  MapBookmarkApp
+//
+//  Created by Anthony Aniobi on 17/01/2026.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name : String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
